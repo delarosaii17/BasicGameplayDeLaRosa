da21089b047da8d2e2fcb6034a39fb11424a94ba
delarosaii17 <tacaltacc@gmail.com>
2023-03-10 13:59:34 -0800

    Finished 2.4 and easy bonus

--- BasicGameplayDeLaRosa/Assets/Scripts/PlayerController.cs
+++ BasicGameplayDeLaRosa/Assets/Scripts/PlayerController.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float verticalInput;
     public float speed = 10.0f;
     public float xRange = 20;
+    public float zRange = 10;
 
-    public GameObject projectilePrefab; 
+    public GameObject projectilePrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -32,12 +34,23 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
-        if (Input.GetKeyDown (KeyCode.Space))
+        if (transform.position.z < -zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
+        }
+        if (transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             // Launch a projectile from the player
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
 
         }
     }
-}
- 
+} 
