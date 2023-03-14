f26a33e85df2906ac6eca0046bfff6b5bf3b9c15
delarosaii17 <tacaltacc@gmail.com>
2023-03-14 15:26:39 -0700

    Finished Challenge II

---
+++ BasicGameplayDeLaRosa/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float SpawnRate = 1f;
+    private float timestamp = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time >= timestamp)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timestamp = Time.time + SpawnRate;
+        }
+    }
+}
